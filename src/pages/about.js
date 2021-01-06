7c5f08770053c58882156e446f0a3c72e4a9549f
maklad42 <maklad42+git@gmail.com>
2021-01-06 21:05:31 +0900

    Create the contact.js file. 210106

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Header from '../components/header';
 
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
+      <Link to="/contact">Contact</Link>
       <Header headerText="About Gatsby!" />
       <Header headerText="Different Header! Cool, huh!" />
       <p>Wow, such a cool framework. And so easy to use!</p>
+      <img src="https:/source.unsplash.com/random/400x200" alt="" />
     </div>
   );
 }
