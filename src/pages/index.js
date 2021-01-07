0b2621f19ef04e96f8998fbef10ee0d947bbf46f
maklad42 <maklad42+git@gmail.com>
2021-01-07 21:52:29 +0900

    Add .src/styles/global.css file and import it into index.js. 210107

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
+import "../styles/global.css"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <div className="main">
       <h1>Hello Gatsby!</h1>
       <p>What's uuuuuppppppppp!</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
